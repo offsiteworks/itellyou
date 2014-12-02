8a4f02949a354f68b82eb6ebdf1fb651f56de67c
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2014-12-02 23:20:22 +0900

    herokuとgitコマンド

---
+++ heroku/git-remote-add-heroku.cmd
@@ -0,0 +1,2 @@
+git remote add heroku git@heroku.com:itellyou.git
+pause
--- heroku/heroku-logs-tail.cmd
+++ heroku/heroku-logs-tail.cmd
@@ -1,4 +1,4 @@
-cd %~dp0
-cd ..
-heroku logs --tail | node -e process.stdin.pipe(process.stdout);
+@cd %~dp0
+@cd ..
+call heroku logs --tail | node -e process.stdin.pipe(process.stdout);
 pause
