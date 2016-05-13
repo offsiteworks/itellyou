6f50a6945b3be82ecc9ed7a964a57938a92b35ca
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2016-05-13 15:12:08 +0900

    2 spaces to tab

--- data/data.js
+++ data/data.js
@@ -1,115 +1,115 @@
 module.exports = {
-  users: [{
-    user_id: "rsuser",
-    name: "ユーザー",
-  }],
-  groups: [{
-    group_id: "rs",
-    name: "両毛システムズ",
-    sites: [{
-      site_id: "site-12345",
-      name: "RS本社ビル",
-      locations: [{
-        location_id: "loc-701",
-        name: "7F男用",
-        rooms: [{
-          room_id: "room-711",
-          name: "1",
-          status: 0,
-        }, {
-          room_id: "room-712",
-          name: "2",
-          status: 0,
-        }],
-      }, {
-        location_id: "loc-601",
-        name: "6F男用",
-        rooms: [{
-          room_id: "room-611",
-          name: "1",
-          status: 0,
-        }, {
-          room_id: "room-612",
-          name: "2",
-          status: 0,
-        }],
-      }, {
-        location_id: "loc-501",
-        name: "5F男用",
-        rooms: [{
-          room_id: "room-511",
-          name: "1",
-          status: 0,
-        }, {
-          room_id: "room-512",
-          name: "2",
-          status: 0,
-        }],
-      }, {
-        location_id: "loc-401",
-        name: "4F男用",
-        rooms: [{
-          room_id: "room-411",
-          name: "1",
-          status: 0,
-        }, {
-          room_id: "room-412",
-          name: "2",
-          status: 0,
-        }],
-      }, {
-        location_id: "loc-301",
-        name: "3F男用",
-        rooms: [{
-          room_id: "room-311",
-          name: "1",
-          status: 0,
-        }, {
-          room_id: "room-312",
-          name: "2",
-          status: 1,
-        }],
-      }, {
-        location_id: "loc-201",
-        name: "2F男用",
-        rooms: [{
-          room_id: "room-211",
-          name: "1",
-          status: 0,
-        }, {
-          room_id: "room-212",
-          name: "2",
-          status: 1,
-        }],
-      }, {
-        location_id: "loc-101",
-        name: "1F男用",
-        rooms: [{
-          room_id: "room-111",
-          name: "1",
-          status: 0,
-        }, {
-          room_id: "room-112",
-          name: "2",
-          status: 0,
-        }],
-      }],
-    }, {
-      site_id: "site-12346",
-      name: "別館",
-      locations: [{
-        location_id: "loc-1501",
-        name: "5F男用",
-        rooms: [{
-          room_id: "room-1511",
-          name: "A",
-          status: 0,
-        }, {
-          room_id: "room-1512",
-          name: "B",
-          status: 1,
-        }],
-      }],
-    }],
-  }],
+	users: [{
+		user_id: "rsuser",
+		name: "ユーザー",
+	}],
+	groups: [{
+		group_id: "rs",
+		name: "両毛システムズ",
+		sites: [{
+			site_id: "site-12345",
+			name: "RS本社ビル",
+			locations: [{
+				location_id: "loc-701",
+				name: "7F男用",
+				rooms: [{
+					room_id: "room-711",
+					name: "1",
+					status: 0,
+				}, {
+					room_id: "room-712",
+					name: "2",
+					status: 0,
+				}],
+			}, {
+				location_id: "loc-601",
+				name: "6F男用",
+				rooms: [{
+					room_id: "room-611",
+					name: "1",
+					status: 0,
+				}, {
+					room_id: "room-612",
+					name: "2",
+					status: 0,
+				}],
+			}, {
+				location_id: "loc-501",
+				name: "5F男用",
+				rooms: [{
+					room_id: "room-511",
+					name: "1",
+					status: 0,
+				}, {
+					room_id: "room-512",
+					name: "2",
+					status: 0,
+				}],
+			}, {
+				location_id: "loc-401",
+				name: "4F男用",
+				rooms: [{
+					room_id: "room-411",
+					name: "1",
+					status: 0,
+				}, {
+					room_id: "room-412",
+					name: "2",
+					status: 0,
+				}],
+			}, {
+				location_id: "loc-301",
+				name: "3F男用",
+				rooms: [{
+					room_id: "room-311",
+					name: "1",
+					status: 0,
+				}, {
+					room_id: "room-312",
+					name: "2",
+					status: 1,
+				}],
+			}, {
+				location_id: "loc-201",
+				name: "2F男用",
+				rooms: [{
+					room_id: "room-211",
+					name: "1",
+					status: 0,
+				}, {
+					room_id: "room-212",
+					name: "2",
+					status: 1,
+				}],
+			}, {
+				location_id: "loc-101",
+				name: "1F男用",
+				rooms: [{
+					room_id: "room-111",
+					name: "1",
+					status: 0,
+				}, {
+					room_id: "room-112",
+					name: "2",
+					status: 0,
+				}],
+			}],
+		}, {
+			site_id: "site-12346",
+			name: "別館",
+			locations: [{
+				location_id: "loc-1501",
+				name: "5F男用",
+				rooms: [{
+					room_id: "room-1511",
+					name: "A",
+					status: 0,
+				}, {
+					room_id: "room-1512",
+					name: "B",
+					status: 1,
+				}],
+			}],
+		}],
+	}],
 }
